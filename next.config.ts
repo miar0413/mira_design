25d0e85b324c016b0f82b3ba045b5769777ed114
ping <kevlinping@gmail.com>
2025-01-25 15:58:30 +0800

    feat: mdx 文件夹

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 添加 mdx 文件扩展名支持
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
 
-export default nextConfig;
+// 创建 MDX 配置
+const withMDX = createMDX({
+  options: {
+    // 如果你想要使用其他插件，可以在这里添加
+    remarkPlugins: [],
+    rehypePlugins: [],
+  }
+})
+
+// 应用 MDX 配置
+export default withMDX(nextConfig)
