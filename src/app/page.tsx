34f49f83a38446be44c088d12a40e9ba03750bbd
ping <kevlinping@gmail.com>
2025-02-08 11:55:06 +0800

    feat: v0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,7 +143,7 @@ const Home: React.FC = () => {
             {/* Hero Section */}
             <div className="flex-grow  text-white relative flex flex-col h-[880px] overflow-hidden">
               <header className="z-20">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                   <div className="flex justify-between items-center h-16">
                     {/* Desktop Navigation */}
                     <nav className="hidden md:flex items-center space-x-1">
